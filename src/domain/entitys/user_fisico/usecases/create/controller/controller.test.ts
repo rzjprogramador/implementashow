435d2a1e0d2900d1c3038d89061a205d22705ca1
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-05-21 22:16:24 -0300

    feat: criacao ok user_fisico - via controller

--- src/domain/entitys/user_fisico/usecases/create/controller/controller.test.ts
+++ src/domain/entitys/user_fisico/usecases/create/controller/controller.test.ts
@@ -1,16 +1,12 @@
-// deno-lint-ignore-file no-explicit-any no-unused-vars
 import { expect } from "https://deno.land/x/expect@v0.2.10/expect.ts";
 
-import { UserFisicoArgs } from "../../../contracts.ts";
 import { controllerCreateUserFisico } from "./controller.ts";
 import { fakeListUserFisicoArgsOK } from "../../../../../../data/literals/user_fisico/fake.ts";
-import { HttpResponse } from "../../../../../../../global/helpers/httpResponse.ts";
 
 const sut = controllerCreateUserFisico;
-
 const inputOK = fakeListUserFisicoArgsOK[0];
 
-Deno.test("[ OK >> ... ] deve retornar as props corretas da criacao OK", async () => {
+Deno.test("deve retornar a criacao OK do model user_fisico com todas as propriedades.", async () => {
   const input = inputOK;
   const actual = await sut(input);
 
@@ -21,13 +17,16 @@ Deno.test("[ OK >> ... ] deve retornar as props corretas da criacao OK", async (
   expect(actual.data).toHaveProperty("email");
   expect(actual.data).toHaveProperty("dataNascimento");
   expect(actual.data).toHaveProperty("tipoUser");
+  expect(actual.data).toHaveProperty("tipoUser");
   expect(actual.data).toHaveProperty("endereco");
 });
 
 /* TESTER_CONSOLE ************************************* */
 
 function show_controllerCreateUserFisico() {
-  console.log(sut(inputOK));
+  sut(inputOK)
+  .then(d => console.log(d))
+  .catch(err => console.error(err.message))
 }
 show_controllerCreateUserFisico();
 
