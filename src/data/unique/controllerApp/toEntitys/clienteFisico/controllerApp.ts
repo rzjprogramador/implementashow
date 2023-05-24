875daddd3424571036763213e841493663221b9d
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-05-24 10:39:21 -0300

    refactor: importacoes

--- src/data/unique/controllerApp/toEntitys/clienteFisico/controllerApp.ts
+++ src/data/unique/controllerApp/toEntitys/clienteFisico/controllerApp.ts
@@ -1,8 +1,14 @@
+import { ClienteFisicoRepository } from "../../../../../domain/entitys/clienteFisico/contracts.ts";
 import { clienteFisicoRepositoryProduction } from "../../../../../frameworks/repository/clienteFisico/repository.ts";
 
-export const clienteFisicoControllerAppRepository = {
-  create: clienteFisicoRepositoryProduction,
-};
+interface ClienteFisicoControllerAppRepository {
+  create: ClienteFisicoRepository;
+}
+
+export const clienteFisicoControllerAppRepository:
+  ClienteFisicoControllerAppRepository = {
+    create: clienteFisicoRepositoryProduction,
+  };
 
 /*
  * 1x1 com useCases
