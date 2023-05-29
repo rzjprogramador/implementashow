7a026e37e6c3233725560fa22d37d7d6e1e8c232
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-05-29 09:30:40 -0300

    feat: deno json por este projeto

--- tester.foo.ts
+++ tester.foo.ts
@@ -1,27 +1,5 @@
-// deno-lint-ignore-file no-explicit-any no-unused-vars
-import { expect } from "https://deno.land/x/expect@v0.2.10/expect.ts";
+const foo = "foo";
 
-const Log = (d: any) => console.log(d);
+const bar = "bar";
 
-const sut = (d: any) => d
-
-Deno.test("[ deve retornar ", () => {
-  const input = "foo";
-  const actual = sut(input);
-  const assertion: any = "foo";
-  // RESULT >>>>>>>>>>>>  ", actual);
-
-  expect(actual).toEqual(assertion);
-});
-
-/* TESTER_CONSOLE ************************************* */
-
-function show_ () {
-  const input = "foo"
-  const res = sut(input);
-
-  console.log(res);
-}
-show_ ();
-
-export default 1;
+console.log(foo);
