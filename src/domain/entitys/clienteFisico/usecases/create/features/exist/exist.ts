c063d970510535428555900774961cafcde8e839
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-05-29 14:59:39 -0300

    refactor: cleancomments in  existClienteFisico

--- src/domain/entitys/clienteFisico/usecases/create/features/exist/exist.ts
+++ src/domain/entitys/clienteFisico/usecases/create/features/exist/exist.ts
@@ -13,22 +13,6 @@ const featExistClienteFisico = async (m: ArgExistClienteFisico) => {
     // Log('ja existe - entao retorno em erro uma excessao')
     throw new Error(feedbacks.alreadyExists)
   }
-
-  // if(typeof operation == 'undefined') {
-  //   // Log('nao existe - entao retorno o dado enviado de  entrada')
-  //   return await m
-  // } else {
-  //   // Log('ja existe - entao retorno em erro uma excessao')
-  //   throw new Error(feedbacks.alreadyExists)
-  // }
-
-  // const response = await mediatorRepositoryClienteFisico?.exist?.exist(m);
-
-  // if (typeof response == "undefined") {
-  //   return m;
-  // } else {
-  //   throw new Error(feedbacks.alreadyExists);
-  // }
 };
 
 export { featExistClienteFisico };
