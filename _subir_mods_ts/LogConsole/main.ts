bbad30736c150463653f607d55c5f86c3fe3e7e0
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-05-29 10:54:02 -0300

    refactor: removendo dir subir_mods enviando para pasta rzModulesTS

--- _subir_mods_ts/LogConsole/main.ts
+++
@@ -1,6 +0,0 @@
-// deno-lint-ignore-file no-explicit-any
-
-
-const Log = (...d: any[]): void => console.log(d)
-
-export { Log }
--- _subir_mods_ts/objectLogs/logUser/controller.ts
+++
@@ -1,7 +0,0 @@
-import { logUserRepositoryProduction } from "../../../src/frameworks/repository/log_user/repository.ts";
-import { repositoryLogUserMock } from "./main.test.ts";
-
-export const repositoryInUselogUser = {
-  production: logUserRepositoryProduction,
-  mock: repositoryLogUserMock,
-}
