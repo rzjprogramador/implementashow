71dd04a6b045a08aa9211680d1445b1484e938ec
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-01 14:10:40 -0300

    refactor: modularizacao import_map - em andamento

--- src/domain/entitys/clienteFisico/features/create/index.ts
+++
@@ -1,7 +0,0 @@
-// @featuresArgsCreateClienteFisico/
-
-export * from "./args/exist/exist.ts";
-export * from "./args/factory/factory.ts";
-export * from "./args/save.ts";
-export * from "./args/prepare.ts";
-export * from "./args/validateArgs.ts";
