31eda5a821745e98182cca71418bdeaaef5c6a7b
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-02 20:59:06 -0300

    refactor: remove extraDeps e subDeps in useCase create

--- src/domain/entitys/clienteFisico/deps.deps.ts
+++ src/domain/entitys/clienteFisico/deps.deps.ts
@@ -1,7 +1,7 @@
-/**
- * DEPS ENTITY
- */
+/** * DEPS ENTITY */
 
+// USECASES ************************************************
+// CREATE
 // FEATURES FACTORY - FOR CREATE ENTITY
 export * from "./useCases/create/features/factory.ts";
 export * from "./useCases/create/features/members.ts";
@@ -12,8 +12,9 @@ export * from "./useCases/create/features/validateArgs.ts";
 
 // MASTER_PIPE_ARGS - CREATE ENTITY
 export * from "./useCases/create/indexPipe.ts";
+// USECASES FINALLY  ***************************************
 
-// DIR_ENTITY
+// DIR_ENTITY **********************************************
 export * from "./uniques/subDeps.deps.ts";
 export * from "./controllers/subDeps.deps.ts";
 export * from "./externalsForEntity/subDeps.deps.ts";
