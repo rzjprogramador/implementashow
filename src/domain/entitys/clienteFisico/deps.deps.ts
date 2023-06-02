1ca3e01bf50697b80aa47b321d9ff14827d6457f
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-02 20:53:20 -0300

    refactor: remove extraDeps e subDeps in useCase create

--- src/domain/entitys/clienteFisico/deps.deps.ts
+++ src/domain/entitys/clienteFisico/deps.deps.ts
@@ -1,22 +1,19 @@
 /**
- * DEPS ENTITY >> clienteFisico >> puxar todos subDeps
+ * DEPS ENTITY
  */
 
-// testar aqui extra deps direto
-export * from "./useCases/create/features/factory.ts"
+// FEATURES FACTORY - FOR CREATE ENTITY
+export * from "./useCases/create/features/factory.ts";
 export * from "./useCases/create/features/members.ts";
 export * from "./useCases/create/features/exist.ts";
 export * from "./useCases/create/features/prepare.ts";
 export * from "./useCases/create/features/save.ts";
 export * from "./useCases/create/features/validateArgs.ts";
 
-// master pipeArgs
-export * from "./useCases/create/indexPipe.ts"
+// MASTER_PIPE_ARGS - CREATE ENTITY
+export * from "./useCases/create/indexPipe.ts";
 
-// ---
-
+// DIR_ENTITY
 export * from "./uniques/subDeps.deps.ts";
 export * from "./controllers/subDeps.deps.ts";
 export * from "./externalsForEntity/subDeps.deps.ts";
-
-// export * from "./useCases/subDeps.deps.ts";
--- src/domain/entitys/clienteFisico/useCases/subDeps.deps.ts
+++
@@ -1,8 +0,0 @@
-/**
- * DEPS ENTITY : clienteFisico >> busca os subDeps de cada useCase
- */
-
-// export * from "./create/extraDeps.deps.ts";
-
-// master pipeArgs
-// export * from "./create/indexPipe.ts";
