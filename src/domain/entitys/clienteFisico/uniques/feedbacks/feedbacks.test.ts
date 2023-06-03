a258437e44a059ae41c797d560b5c41d518a88e6
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-03 18:52:48 -0300

    refactor: clean

--- src/domain/entitys/clienteFisico/uniques/feedbacks/feedbacks.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from "https://deno.land/x/expect@v0.2.10/expect.ts";
-
-import { feedbackClienteFisico } from "../../clienteFisico.deps.ts";
-
-const sut = feedbackClienteFisico;
-
-Deno.test("deve retornar os feedbacks corretamente", () => {
-  expect(sut.createClienteFisicoOk()).toBe("Usuario Fisico criado com sucesso");
-});
