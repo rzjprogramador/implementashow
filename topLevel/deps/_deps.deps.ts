05409a1cb00050e5c462b88b883905cf0a3a3c74
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-03 13:17:23 -0300

    refactor:manutencao mudar de pasta o artefato manutencao somente no topLevelDeps

--- topLevel/deps/_deps.deps.ts
+++ topLevel/deps/_deps.deps.ts
@@ -1,10 +1,13 @@
 /** *
  *  DEPS ENTITY
  *  REGISTRE E DEPOIS RECUPERE TODOS ARTEFATOS DO MODULO AQUI.
- * importante: ['ordemDeProcedencia do Uso é importante', ]
- * */
+ * manutencaoMudancas: [
+  [x] se mudar de pasta >> manutencao somente aqui
+  [x] se mudar de pasta >> manutencao somente aqui
+ ],
+ */
 
-// EXTERNALS FOR ENTITY **********************************************
+
 // externals
 export * from "./../../src/externals/modules/rzModulesTS.ts";
 
@@ -12,69 +15,33 @@ export * from "./../../src/externals/modules/rzModulesTS.ts";
 export * from "./../../src/domain/entitys/clienteFisico/uniques/contracts/contracts.ts";
 
 // create - features
-export * from "./../../src/domain/entitys/clienteFisico/useCases/create/features/exist.ts"
-export * from "./../../src/domain/entitys/clienteFisico/useCases/create/features/factory.ts"
-export * from "./../../src/domain/entitys/clienteFisico/useCases/create/features/members.ts"
-export * from "./../../src/domain/entitys/clienteFisico/useCases/create/features/prepare.ts"
-export * from "./../../src/domain/entitys/clienteFisico/useCases/create/features/save.ts"
-export * from "./../../src/domain/entitys/clienteFisico/useCases/create/features/validateArgs.ts"
+export * from "./../../src/domain/entitys/clienteFisico/useCases/create/features/exist.ts";
+export * from "./../../src/domain/entitys/clienteFisico/useCases/create/features/factory.ts";
+export * from "./../../src/domain/entitys/clienteFisico/useCases/create/features/members.ts";
+export * from "./../../src/domain/entitys/clienteFisico/useCases/create/features/prepare.ts";
+export * from "./../../src/domain/entitys/clienteFisico/useCases/create/features/save.ts";
+export * from "./../../src/domain/entitys/clienteFisico/useCases/create/features/validateArgs.ts";
 // masterPipeArgs
-export * from "./../../src/domain/entitys/clienteFisico/useCases/create/masterPipeArgs.ts"
+export * from "./../../src/domain/entitys/clienteFisico/useCases/create/masterPipeArgs.ts";
 
 // externalsForEntity
-export * from "./../../src/domain/entitys/clienteFisico/externalsForEntity/repositoryEntityProduction/repositoryProduction.ts"
-export * from "./../../src/domain/entitys/clienteFisico/externalsForEntity/repositoryEntityProduction/repositoryProduction.ts"
-export * from "./../../src/domain/entitys/clienteFisico/externalsForEntity/repositoryLoog/repositoryLoog.ts"
+export * from "./../../src/domain/entitys/clienteFisico/externalsForEntity/repositoryEntityProduction/repositoryProduction.ts";
+export * from "./../../src/domain/entitys/clienteFisico/externalsForEntity/repositoryEntityProduction/repositoryProduction.ts";
+export * from "./../../src/domain/entitys/clienteFisico/externalsForEntity/repositoryLoog/repositoryLoog.ts";
 
 // helpers global entitys
-export * from "./../../src/domain/helpers/feedbacks/feedbacks.ts"
-export * from "./../../src/domain/helpers/httpResponse/httpResponse.ts"
-
+export * from "./../../src/domain/helpers/feedbacks/feedbacks.ts";
+export * from "./../../src/domain/helpers/httpResponse/httpResponse.ts";
 
 // controller
-export * from "./../../src/domain/entitys/clienteFisico/controllers/create/controller.ts"
+export * from "./../../src/domain/entitys/clienteFisico/controllers/create/controller.ts";
 
 // uniques
-export * from "./../../src/domain/entitys/clienteFisico/uniques/fakes/fakes.ts"
-export * from "./../../src/domain/entitys/clienteFisico/uniques/feedbacks/feedbacks.ts"
-export * from "./../../src/domain/entitys/clienteFisico/uniques/mediators/mediators.ts"
-export * from "./../../src/domain/entitys/clienteFisico/uniques/seeds/seeds.ts"
 
+// export * from "./../../src/domain/entitys/clienteFisico/uniques/fakes/fakes.ts" // fake
 
+export * from "../../foo/fakes/fakes.ts"; // fake
 
-
-
-
-
-
-// externalsForEntity/repositoryEntityProduction/repositoryProduction.ts
-
-/*
-export * from "./externalsForEntity/repositoryLoog/repositoryLoog.ts";
-
-// USECASES ************************************************
-// CREATE
-// FEATURES FACTORY - FOR CREATE ENTITY
-export * from "./useCases/create/features/factory.ts";
-export * from "./useCases/create/features/members.ts";
-export * from "./useCases/create/features/exist.ts";
-export * from "./useCases/create/features/prepare.ts";
-export * from "./useCases/create/features/save.ts";
-export * from "./useCases/create/features/validateArgs.ts";
-
-// MASTER_PIPE_ARGS - CREATE ENTITY
-export * from "./useCases/create/masterPipeArgs.ts";
-// USECASES FINALLY  ***************************************
-
-// USECASES CONTROLLER **********************************************
-export * from "./../clienteFisico/controllers/create/controller.ts";
-
-// DIRETORIES_AUX ENTITY **********************************************
-// UNIQUES
-export * from "../clienteFisico/uniques/contracts/contracts.ts";
-export * from "../clienteFisico/uniques/fakes/fakes.ts";
-export * from "../clienteFisico/uniques/feedbacks/feedbacks.ts";
-export * from "../clienteFisico/uniques/mediators/mediators.ts";
-export * from "../clienteFisico/uniques/seeds/seeds.ts";
-
-*/
+export * from "./../../src/domain/entitys/clienteFisico/uniques/feedbacks/feedbacks.ts";
+export * from "./../../src/domain/entitys/clienteFisico/uniques/mediators/mediators.ts";
+export * from "./../../src/domain/entitys/clienteFisico/uniques/seeds/seeds.ts";
