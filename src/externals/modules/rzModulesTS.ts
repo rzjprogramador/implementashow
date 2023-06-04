b0072456a01b19e0e237f6f3891958a0cf247d54
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-04 12:40:06 -0300

    feat: replicando modulos externos com prefixo mke de forma curta

--- src/externals/modules/rzModulesTS.ts
+++ src/externals/modules/rzModulesTS.ts
@@ -1,26 +1,24 @@
 // deno-lint-ignore-file no-unused-vars
 
 // dateMomentRZ ============================================================
-
-// TUDO DE dateMomentRZ
-import { objectDateCurrent } from "/home/rzj/..gitAll/rzModulesTS_FIXOPC/dateMomentRZ/index.ts";
-export const mkeObjectDateCurrent = objectDateCurrent;
+export { objectDateCurrent as mkeObjectDateCurrent } from "/home/rzj/..gitAll/rzModulesTS_FIXOPC/dateMomentRZ/index.ts";
 
 // devToolsRZ ============================================================
-import { Log } from "/home/rzj/..gitAll/rzModulesTS_FIXOPC/devToolsRZ/index.ts";
-export const mkeLog = Log;
+export { Log as mkeLog } from "/home/rzj/..gitAll/rzModulesTS_FIXOPC/devToolsRZ/index.ts";
 
 // appUserRZ ============================================================
-import {
-  ILoggPersonModel,
-  ILoggUserType,
-} from "/home/rzj/..gitAll/rzModulesTS_FIXOPC/appUserRZ/loggUser/index.ts";
 export type {
   ILoggPersonModel as ImkeLoggPersonModel,
   ILoggUserType as ImkeLoggUserType,
-};
+} from "/home/rzj/..gitAll/rzModulesTS_FIXOPC/appUserRZ/loggUser/index.ts";
 
 // objectValuesFN ============================================================
 export { objectValuesFN as mkeObjectValuesFN } from "https://raw.githubusercontent.com/rzjprogramador/lib_rzj_typescript/v1.0.0_objectsValueFN/main.ts";
 
-/* */
+/*
+
+import_export_replicar_modulos: "ao inves de importar e depois criar nova var para uso das replicas, faz assim : exporta o endereco do modulo que traz tudo, , { extrai os artefatosAlvo as mkeNovaVariavel_ParaReplicaDeUso }"
+
+info: "as variaveis de replica após a keyword <as> são variaveis novas e elas que sera usadas pela app."
+
+*/
