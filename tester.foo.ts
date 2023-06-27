1d234ab9553be640364454e9c9f13e37394218fa
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-27 12:00:15 -0300

    refactor: reestruturacao

---
+++ tester.foo.ts
@@ -0,0 +1,4 @@
+import { SuperValidateTS } from "/home/rzj/..gitAll/implementashow/roots/rootDeps/group/replicasExternalDeps.ts";
+
+// tester uso minString  - formar ObjectValue
+console.log(SuperValidateTS.minString('o'))
