d94f0c6e369ba12e356c17b0365910f02a0cf904
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-07-02 18:03:09 -0300

    refactor: okok

--- tester.foo.ts
+++
@@ -1,4 +0,0 @@
-import { SuperValidateTS } from "/home/rzj/..gitAll/implementashow/roots/rootDeps/group/replicasExternalDeps.ts";
-
-// tester uso minString  - formar ObjectValue
-console.log(SuperValidateTS.minString('o'))
