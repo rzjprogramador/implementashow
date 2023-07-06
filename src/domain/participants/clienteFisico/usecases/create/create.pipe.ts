71ca2c9b2e9038b746636c64d25e0f860f518908
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-07-06 13:01:47 -0300

    refactor: modificado o pipe para pipe generico

--- src/domain/participants/clienteFisico/usecases/create/create.pipe.ts
+++ src/domain/participants/clienteFisico/usecases/create/create.pipe.ts
@@ -3,16 +3,16 @@ import {
   listFeatArgsClienteFisico,
   type ArgsClienteFisico,
 } from "@clienteFisico";
+import { PipeGeneric } from "@replicasRemote"
 
 /**
- * @description acoes realizadas com o input de argumentos, para serem usadas no usecase -> create
+ * @description configuracao passe o inputArgtarget e receber este inputArgtarget tratado.
  * @actions_in_args 1- validar_argumentos +, 2- fabricarEntity +
  * @returns daqui sai os argumentos validos para o objetivo de criacao no controle.
  */
 
-const pipecaseCreateClienteFisico = async (target: ArgsClienteFisico) => {
-  return listFeatArgsClienteFisico.reduce((acc: any, fn) => fn(acc), target)
-}
+const pipecaseCreateClienteFisico = async (argTarget: ArgsClienteFisico) => await PipeGeneric<ArgsClienteFisico, ArgsClienteFisico>(argTarget, listFeatArgsClienteFisico)
+
 
 export {
   pipecaseCreateClienteFisico
